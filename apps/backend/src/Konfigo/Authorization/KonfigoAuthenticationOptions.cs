fa9c3d4a86c13bc16371ea63e82cfe7dca75a303
makszimin <zim.ma2010@yandex.ru>
2026-06-22 19:48:29 +0200

    Set default authentication provider to OpenId and update test config

--- apps/backend/src/Konfigo/Authorization/KonfigoAuthenticationOptions.cs
+++ apps/backend/src/Konfigo/Authorization/KonfigoAuthenticationOptions.cs
@@ -5,7 +5,7 @@ namespace Konfigo.Authorization;
 public sealed class KonfigoAuthenticationOptions
 {
     public const string SectionName = "Authentication";
-    public AuthenticationProvider Provider { get; init; } = AuthenticationProvider.Saml;
+    public AuthenticationProvider Provider { get; init; } = AuthenticationProvider.OpenId;
 
     public string IdClaimType { get; init; } = ClaimTypes.NameIdentifier;
     public string RoleClaimType { get; init; } = ClaimsIdentity.DefaultRoleClaimType;
